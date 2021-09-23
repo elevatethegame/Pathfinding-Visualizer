73b300dce521222f45ec6444600d6ec678937521
Bruce Ong <bruceskellator@hotmail.com>
2021-09-23 13:25:44 +0800

    Finished styling Navbar component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Grid from './Grid'
+import Navbar from './Navbar'
+import Statbar from './Statbar'
 
 function Menu() {
     return (
         <div>
-            
+            <Navbar />
+            <Grid />
+            <Statbar />
         </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import '../css/Navbar.css'
 
 function Navbar() {
     return (
-        <nav>
-
+        <nav className='navbar'>
+            <div className='navbar-list-container'>
+                <ul className='navbar-list'>
+                    <li className='navbar-item'>
+                        <button className='navbar-button navbar-normal-button'>Generate Walls</button>
+                    </li>
+                    <li className='navbar-item'>
+                        <button  className='navbar-button navbar-normal-button'>Select Algorithm</button>
+                    </li>
+                    <li className='navbar-item'>
+                        <button disabled={'true'} className='navbar-button navbar-run-button'>Run</button>
+                    </li>  
+                    <li className='navbar-item'>
+                        <button  className='navbar-button navbar-normal-button'>Clear Path</button>
+                    </li>
+                    <li className='navbar-item'>
+                        <button  className='navbar-button navbar-normal-button'>Clear Board</button>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
