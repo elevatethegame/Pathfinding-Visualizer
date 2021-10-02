2c4816ac5819567cee31b7581bd5a3d5ab734284
Bruce Ong <bruceskellator@hotmail.com>
2021-10-02 21:47:56 +0800

    Implemented Node component render logic

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,12 +1,42 @@
 import React from 'react'
 import '../css/Node.css'
+import { connect } from 'react-redux'
 
-function Node() {
+function Node({isVisitedNode, isWallNode, isEndNode, isStartNode, isFrontierNode, isPathNode}) {
+
+    let nodeClass
+    if (isStartNode) {
+        nodeClass = 'start'
+    } else if (isEndNode) {
+        nodeClass = 'end'
+    } else if (isWallNode) {
+        nodeClass = 'wall'
+    } else if (isPathNode) {
+        nodeClass = 'path'
+    } else if (isVisitedNode) {
+        nodeClass = 'visited'
+    } else if (isFrontierNode) {
+        nodeClass = 'frontier'
+    } else {
+        nodeClass = 'unvisited'
+    }
+
     return (
-        <div className='node unvisited'>
+        <div className={`node ${nodeClass}`}>
             
         </div>
     )
 }
 
-export default Node
+const mapStateToProps = (state, ownProps) => {
+    return {
+        isVisitedNode: state.board.grid[ownProps.row][ownProps.col].isVisitedNode,
+        isWallNode: state.board.grid[ownProps.row][ownProps.col].isWallNode,
+        isStartNode: state.board.grid[ownProps.row][ownProps.col].isStartNode,
+        isEndNode: state.board.grid[ownProps.row][ownProps.col].isEndNode,
+        isPathNode: state.board.grid[ownProps.row][ownProps.col].isPathNode,
+        isFrontierNode: state.board.grid[ownProps.row][ownProps.col].isFrontierNode,
+    }
+}
+
+export default connect(mapStateToProps)(Node)
